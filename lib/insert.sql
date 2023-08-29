22c4f697fa5f546064258b383d6dc8ed57df1620
Cnnjuguna <123736824+Cnnjuguna@users.noreply.github.com>
2023-08-29 18:21:19 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,30 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    "Mr. Chocolate", 3, "M", "Chocolate", "affectionate", 1
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    "Rowdy", 3, "M", "Chocolate", "protective", 1
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+        "Sergeant Brown", 3, "M", "Chocolate", "strict", 1
+
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+        "Melissa", 3, "M", "Chocolate", "playful", 1
+
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+        "Tabitha", 3, "M", "Chocolate", "altruistic", 1
+
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+        "Grinch", 3, "M", "Chocolate", "fearful", 1
+
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+        "Wendy", 3, "M", "Chocolate", "social", 1
+
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+        NULL, 3, "M", "Chocolate", "social", 1
+
+);
